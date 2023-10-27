9023ce3386b7c69a093926fc5df02c45911506aa
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-10-27 15:39:33 +0800

    feat(toggle header): fix extension error

--- lua/simple-scripts.lua
+++ lua/simple-scripts.lua
@@ -1,25 +1,23 @@
 local M = {}
 
-local source_ext = { ".cpp", ".cc", ".c" }
-local header_ext = { ".h", ".hpp" }
+local source_ext = { "cpp", "cc", "c" }
+local header_ext = { "h", "hpp" }
 
 M.toggle = function()
 	local filename = vim.fn.expand("%:t:r")
 	local extension = vim.fn.expand("%:e")
 
-	print(filename .. extension)
-
 	if vim.tbl_contains(source_ext, extension) then
 		for _, ext in ipairs(source_ext) do
-			if vim.fn.filereadable(filename .. ext) == 1 then
-				vim.cmd("find " .. filename .. ext)
+			if vim.fn.filereadable(filename .. "." .. ext) == 1 then
+				vim.cmd("find " .. filename .. "." .. ext)
 				return
 			end
 		end
 	elseif vim.tbl_contains(header_ext, extension) then
 		for _, ext in ipairs(header_ext) do
-			if vim.fn.filereadable(filename .. ext) == 1 then
-				vim.cmd("find " .. filename .. ext)
+			if vim.fn.filereadable(filename .. "." .. ext) == 1 then
+				vim.cmd("find " .. filename .. "." .. ext)
 				return
 			end
 		end
