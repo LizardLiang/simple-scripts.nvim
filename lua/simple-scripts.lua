67c5045b6589aac2219d985573e36584851a8018
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-10-27 15:50:19 +0800

    impr(toggle header): probably can enhance performance

--- lua/simple-scripts.lua
+++ lua/simple-scripts.lua
@@ -3,14 +3,14 @@ local M = {}
 M.toggle = function()
 	local filename = vim.fn.expand("%:t:r")
 	local extension = vim.fn.expand("%:e")
-	local dir = vim.fn.expand("%:p:h") .. "/"
+	local dir = vim.fn.expand("%:p:h")
 
 	local source_extensions = { "cpp", "cc", "c" }
 	local header_extensions = { "h", "hpp", "hxx" }
 
-	local function find_file_in_dir_and_subdir(filename, extensions)
+	local function find_and_open_file(dir, filename, extensions)
 		for _, ext in ipairs(extensions) do
-			local found_files = vim.fn.glob(dir .. "**/" .. filename .. "." .. ext)
+			local found_files = vim.fn.glob(dir .. "/" .. filename .. "." .. ext)
 			if found_files ~= "" then
 				local first_found = string.match(found_files, "[^\n]+")
 				vim.cmd("edit " .. first_found)
@@ -20,10 +20,22 @@ M.toggle = function()
 		return false
 	end
 
+	local function switch_dir(dir)
+		if string.find(dir, "/source/") then
+			return string.gsub(dir, "/source/", "/include/")
+		elseif string.find(dir, "/include/") then
+			return string.gsub(dir, "/include/", "/source/")
+		else
+			return dir
+		end
+	end
+
+	local target_dir = switch_dir(dir)
+
 	if vim.tbl_contains(source_extensions, extension) then
-		find_file_in_dir_and_subdir(filename, header_extensions)
+		find_and_open_file(target_dir, filename, header_extensions)
 	elseif vim.tbl_contains(header_extensions, extension) then
-		find_file_in_dir_and_subdir(filename, source_extensions)
+		find_and_open_file(target_dir, filename, source_extensions)
 	end
 end
 
