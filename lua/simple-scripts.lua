88d2dbc9098398fea299087d4ff7cd7888048da0
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-10-27 15:41:05 +0800

    fix(toggle header): use wrong extension set

--- lua/simple-scripts.lua
+++ lua/simple-scripts.lua
@@ -8,14 +8,14 @@ M.toggle = function()
 	local extension = vim.fn.expand("%:e")
 
 	if vim.tbl_contains(source_ext, extension) then
-		for _, ext in ipairs(source_ext) do
+		for _, ext in ipairs(header_ext) do
 			if vim.fn.filereadable(filename .. "." .. ext) == 1 then
 				vim.cmd("find " .. filename .. "." .. ext)
 				return
 			end
 		end
 	elseif vim.tbl_contains(header_ext, extension) then
-		for _, ext in ipairs(header_ext) do
+		for _, ext in ipairs(source_ext) do
 			if vim.fn.filereadable(filename .. "." .. ext) == 1 then
 				vim.cmd("find " .. filename .. "." .. ext)
 				return
