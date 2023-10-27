ef125c4a67a68bcb062d0a0374dcc8c8ebe6d7a7
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-10-27 15:34:10 +0800

    feat(toggle header): find all possible extensions

--- lua/simple-scripts.lua
+++ lua/simple-scripts.lua
@@ -1,12 +1,25 @@
 local M = {}
 
+local source_ext = { ".cpp", ".cc", ".c" }
+local header_ext = { ".h", ".hpp" }
+
 M.toggle = function()
 	local filename = vim.fn.expand("%:t:r")
 	local extension = vim.fn.expand("%:e")
-	if extension == "cpp" then
-		vim.cmd("find " .. filename .. ".h")
-	elseif extension == "h" then
-		vim.cmd("find " .. filename .. ".cpp")
+	if vim.tbl_contains(source_ext, extension) then
+		for _, ext in ipairs(source_ext) do
+			if vim.fn.filereadable(filename .. ext) == 1 then
+				vim.cmd("find " .. filename .. ext)
+				return
+			end
+		end
+	elseif vim.tbl_contains(header_ext, extension) then
+		for _, ext in ipairs(header_ext) do
+			if vim.fn.filereadable(filename .. ext) == 1 then
+				vim.cmd("find " .. filename .. ext)
+				return
+			end
+		end
 	end
 end
 
