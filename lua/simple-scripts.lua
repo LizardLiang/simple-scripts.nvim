ed4b548ee2df4b4f86003ad6ff87312bf7d5c47b
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-10-27 15:43:36 +0800

    feat(toggle header): path should include the sub dir

--- lua/simple-scripts.lua
+++ lua/simple-scripts.lua
@@ -1,26 +1,29 @@
 local M = {}
 
-local source_ext = { "cpp", "cc", "c" }
-local header_ext = { "h", "hpp" }
-
 M.toggle = function()
 	local filename = vim.fn.expand("%:t:r")
 	local extension = vim.fn.expand("%:e")
+	local dir = vim.fn.expand("%:p:h") .. "/"
 
-	if vim.tbl_contains(source_ext, extension) then
-		for _, ext in ipairs(header_ext) do
-			if vim.fn.filereadable(filename .. "." .. ext) == 1 then
-				vim.cmd("find " .. filename .. "." .. ext)
-				return
+	local source_extensions = { "cpp", "cc", "c" }
+	local header_extensions = { "h", "hpp", "hxx" }
+
+	local function find_file_in_dir_and_subdir(filename, extensions)
+		for _, ext in ipairs(extensions) do
+			local found_files = vim.fn.glob(dir .. "**/" .. filename .. "." .. ext)
+			if found_files ~= "" then
+				local first_found = string.match(found_files, "[^\n]+")
+				vim.cmd("edit " .. first_found)
+				return true
 			end
 		end
-	elseif vim.tbl_contains(header_ext, extension) then
-		for _, ext in ipairs(source_ext) do
-			if vim.fn.filereadable(filename .. "." .. ext) == 1 then
-				vim.cmd("find " .. filename .. "." .. ext)
-				return
-			end
-		end
+		return false
+	end
+
+	if vim.tbl_contains(source_extensions, extension) then
+		find_file_in_dir_and_subdir(filename, header_extensions)
+	elseif vim.tbl_contains(header_extensions, extension) then
+		find_file_in_dir_and_subdir(filename, source_extensions)
 	end
 end
 
