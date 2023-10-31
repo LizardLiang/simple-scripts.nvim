a79c155b37a8ca2f7332fbf46f097fb1beff7e0e
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-10-31 16:30:29 +0800

    debug

--- lua/simple-scripts/goto_css_definition.lua
+++ lua/simple-scripts/goto_css_definition.lua
@@ -88,7 +88,7 @@ local function find_full_expression(node)
 			node = parent
 		elseif parent and parent:type() == "member_expression" then
 			node = parent
-		elseif node:type() == "string" and parent:type() == "subscript_expression" then
+		elseif parent:type() == "string" and parent:parent():type() == "subscript_expression" then
 			node = parent
 		else
 			break
